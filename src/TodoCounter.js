5b818fa391b869d808b626f4844851692cf7b5d6
Edward Alexis Ortiz <edaortiz@bancolombia.com.co>
2023-08-13 07:55:31 -0500

    Added todoCounter styles

--- src/TodoCounter.js
+++ src/TodoCounter.js
@@ -2,11 +2,10 @@ import './TodoCounter.css';
 
 function TodoCounter({ total, completed }) {
   return (
-    <h1 >
-      You have completed <span className='totalLabel'>{completed}</span> of <span className='totalLabel'>{total}</span> TODOS
+    <h1 className='todoCounter'>
+      You have completed <span >{completed}</span> of <span >{total}</span> TODOS
     </h1>
   );
 }
 
 export { TodoCounter };
- 
