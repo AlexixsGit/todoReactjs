ba1318899e9bbbc0774946129ee1c953eb4eb9bb
Edward Alexis Ortiz <edaortiz@bancolombia.com.co>
2023-08-13 09:34:21 -0500

    Added new styles to the list

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import './TodoItem.css'
-import { FaCheck, FaRegCheckCircle  } from 'react-icons/fa';
-import { AiFillDelete } from "react-icons/ai";
-import 'bootstrap/dist/css/bootstrap.min.css'; // Make sure this line is included here
+
 
 export function TodoItem({ text, completed }) {
-  return (
-    <li className="todoItem">
-      <span className='Icon Icon-check Icon-check--active'>V</span>
-      <p className='TodoItem-p TodoItem-p--complete'>{text}</p>
-      <span className='Icon Icon-delete'>X</span>
-    </li>
+  return (<li className="todo-item">
+    <button
+      className={`check ${completed && "check--active"}`}
+    ></button>
+    <p>{text}</p>
+    <button className="close-item"></button>
+  </li>
   );
 }
 
