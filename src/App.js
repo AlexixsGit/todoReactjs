fb963a3797c6544d5769564811863c4fbf1a6eef
Edward Alexis Ortiz <edaortiz@bancolombia.com.co>
2023-08-15 21:08:38 -0500

    Search component working

--- src/App.js
+++ src/App.js
@@ -12,22 +12,29 @@ const defaultTodos = [
   { id: 1, text: "Study english", completed: false },
   { id: 2, text: "Learn react js", completed: false },
   { id: 3, text: "Study english", completed: true },
-  { id: 4, text: "Learn react js", completed: false }
+  { id: 4, text: "Learn react js", completed: true  }
 
 ];
 
 function App() {
+  const [todos, setTodos] = React.useState(defaultTodos);
   const [searchValue, setSearchValue] = React.useState('');
+
+  const completedTodos = todos.filter(todo => !!todo.completed).length;
+  const totalTodos = todos.length;
+
+  
+
 
   return (
     <React.Fragment>
-      <TodoCounter completed={10} total={20} />
+      <TodoCounter completed={completedTodos} total={totalTodos} />
       <TodoSearch searchValue={searchValue}
         setSearchValue={setSearchValue} />
       <div className="row justify-content-center">
         <div className="col-md-8">
           <TodoList>
-            {defaultTodos.map(todo => (
+            {todos.filter(todo=>todo.text.toLocaleLowerCase().includes(searchValue)).map(todo => (
               <TodoItem key={todo.id} text={todo.text} completed={todo.completed} />
             ))}
           </TodoList>
