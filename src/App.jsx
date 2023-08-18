8e05f31d6ad1132e4a2f41a0113deef88695263d
Edward Alexis Ortiz <edaortiz@bancolombia.com.co>
2023-08-18 14:31:50 -0500

    I change the extension of the js files to jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,27 +6,9 @@ import { TodoItem } from "./TodoItem/TodoItem";
 import './App.css'
 import React from "react";
 import { CreateTodoButton } from "./CreateTodoButton/CreateTodoButton";
-
-function useLocalStorage(itemName, initialValue) {
-  const localStorageItem = localStorage.getItem(itemName);
-
-  let parsedItem;
-
-  if (!localStorageItem) {
-    localStorage.setItem(itemName, JSON.stringify(initialValue));
-    parsedItem = initialValue;
-  } else {
-    parsedItem = JSON.parse(localStorageItem)
-  }
-  const [item, setItem] = React.useState(parsedItem);
+import { useLocalStorage } from "./hooks/useLocalStorage";
 
 
-  const saveItem = (newItem) => {
-    localStorage.setItem(itemName, JSON.stringify(newItem));
-    setItem(newItem);
-  }
-  return [item, saveItem];
-}
 
 function App() {
 
