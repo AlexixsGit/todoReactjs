891a5e8a7567b90300eb6747eaa99f175abae507
Edward Alexis Ortiz <edaortiz@bancolombia.com.co>
2023-08-22 15:27:34 -0500

    Added the function to close the modal when click it

--- src/TodoForm/TodoForm.jsx
+++ src/TodoForm/TodoForm.jsx
@@ -1,8 +1,20 @@
 import "./TodoForm.css"
+import React from "react";
+import { TodoContext } from "../TodoContext/TodoContext";
 
 export function TodoForm() {
+
+    const {
+        setOpenModal
+    } = React.useContext(TodoContext);
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+        setOpenModal(false);
+    };
+
     return (
-        <form>
+        <form onSubmit={onSubmit}>
             <label>Add your new TODO</label>
             <textarea placeholder="Study english for the exam"></textarea>
             <div className="todo-form-button-container">
