e30da2bcff88291b528d5958fcc08846cde736c8
Edward Alexis Ortiz <edaortiz@bancolombia.com.co>
2023-08-22 11:08:11 -0500

    I created the css for the modal

--- src/Modal/Modal.jsx
+++ src/Modal/Modal.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./Modal.css"
 
 export function Modal({ children }) {
     return ReactDOM.createPortal(
-        <div className="Modal">
+        <div className="modal-background">
             {children}
         </div>,
         document.getElementById('modal')
